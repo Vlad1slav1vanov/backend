dc3f061d43b6362763d7fa8792cc4bc6b8c2d3c3
Vladislav <typlomi@gmail.com>
2022-12-19 22:36:53 +0600

    обработка ошибок пост запросов

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import Post from './post.js';
 
 const PORT = 9000;
 const DB_URL = 'mongodb+srv://typlomi:72ipinip@cluster0.3nz8cjz.mongodb.net/?retryWrites=true&w=majority'
@@ -8,10 +9,14 @@ const app = express();
 
 app.use(express.json())
 
-app.post('/', (req, res) => {
-  console.log(req.body)
-
-  res.status(200).json('Сервер работает')
+app.post('/', async (req, res) => {
+  try {
+    const {author, title, content, picture} = req.body;
+    const post = await Post.create({author, title, content, picture})
+    res.json(post)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 })
 
 async function startApp() {
