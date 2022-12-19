b46bb57d2e31a687805d02aaf7a38f5326a9faf9
Vladislav <typlomi@gmail.com>
2022-12-20 01:17:10 +0600

    Сreate service

--- PostController.js
+++ PostController.js
@@ -1,10 +1,11 @@
 import Post from './Post.js';
+import PostService from './PostService.js';
 
 class PostController {
   async create(req, res) {
     try {
-      const {author, title, content, picture} = req.body;
-      const post = await Post.create({author, title, content, picture})
+      console.log(req.files)
+      const post = await PostService.create(req.body)
       res.json(post)
     } catch (err) {
       res.status(500).json(err)
@@ -13,7 +14,7 @@ class PostController {
 
   async getAll(req, res) {
     try {
-      const posts = await Post.find();
+      const posts = await PostService.getAll();
       return res.json(posts)
     } catch (err) {
       res.status(500).json(err)
@@ -22,11 +23,7 @@ class PostController {
 
   async getOne(req, res) {
     try {
-      const {id} = req.params;
-      const post = await Post.findById(id);
-      if (!id) {
-        res.status(400).json({message: 'Id не указан'})
-      }
+      const post = await PostService.getOne(req.params.id);
       return res.json(post)
     } catch (err) {
       res.status(500).json(err)
@@ -35,24 +32,16 @@ class PostController {
 
   async update(req, res) {
     try {
-      const post = req.body
-      if (!post._id) {
-        res.status(400).json({message: 'Id не указан'})        
-      }
-      const updatedPost = await Post.findByIdAndUpdate(post._id, post, {new: true})
+      const updatedPost = await PostService.update(req.body)
       return res.json(updatedPost)
     } catch (err) {
-      res.status(500).json(err)
+      res.status(500).json(err.message)
     }
   }
 
   async delete(req, res) {
     try {
-      const {id} = req.params
-      if (!id) {
-        res.status(400).json({message: 'Id не указан'})        
-      }
-      const post = await Post.findByIdAndDelete(id)
+      const post = await PostService.delete(req.params.id)
       return res.json(post)
     } catch (err) {
       res.status(500).json(err)
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import router from './router.js';
+import fileUpload from 'express-fileupload';
 
 const PORT = 9000;
 const DB_URL = 'mongodb+srv://typlomi:72ipinip@cluster0.3nz8cjz.mongodb.net/?retryWrites=true&w=majority'
@@ -9,6 +10,7 @@ const app = express();
 
 app.use(express.json())
 app.use('/api', router)
+app.use(fileUpload({}))
 
 async function startApp() {
   try {
