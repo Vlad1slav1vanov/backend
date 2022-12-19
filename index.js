df78e5b0e04d4cd3f63b60f944aa3e926bb6aaff
Vladislav <typlomi@gmail.com>
2022-12-19 22:18:03 +0600

    connect MongoDB

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const PORT = 9000;
+const DB_URL = 'mongodb+srv://typlomi:72ipinip@cluster0.3nz8cjz.mongodb.net/?retryWrites=true&w=majority'
 
 const app = express();
 
@@ -12,4 +14,15 @@ app.post('/', (req, res) => {
   res.status(200).json('Сервер работает')
 })
 
-app.listen(PORT, () => console.log('SERVER STARTED ON PORT ' + PORT));
+async function startApp() {
+  try {
+    await mongoose.connect(DB_URL, {useUnifiedTopology: true, useNewUrlParser: true});
+    app.listen(PORT, () => console.log('SERVER STARTED ON PORT ' + PORT));
+  }
+
+  catch (err) {
+    console.log(err)
+  }
+}
+
+startApp()
