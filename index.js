eaa0ed17304f35fd89a75ad6802c12e63f808976
Vladislav <typlomi@gmail.com>
2022-12-19 22:10:10 +0600

    connect mongoDB

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('SERVER WORKING')
+import express from 'express';
+
+const PORT = 9000;
+
+const app = express();
+
+app.use(express.json())
+
+app.post('/', (req, res) => {
+  console.log(req.body)
+
+  res.status(200).json('Сервер работает')
+})
+
+app.listen(PORT, () => console.log('SERVER STARTED ON PORT ' + PORT));
